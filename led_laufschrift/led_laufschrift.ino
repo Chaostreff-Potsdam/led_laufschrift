01cdbae39cbbbf0a37a627b2fa8b762bf5a6ad2e
Matthias Jacob <dev@matthiasjacob.net>
2023-01-17 00:01:26 +0100

    move boilerplate timer code into header file

--- led_laufschrift/led_laufschrift.ino
+++ led_laufschrift/led_laufschrift.ino
@@ -30,48 +30,13 @@ uint8_t values[COLS];
 
 char myMessage[8];
 
+#include "timer.h"
 
-
-#if !( defined(ARDUINO_SAMD_ZERO) || defined(ARDUINO_SAMD_MKR1000) || defined(ARDUINO_SAMD_MKRWIFI1010) \
-      || defined(ARDUINO_SAMD_NANO_33_IOT) || defined(ARDUINO_SAMD_MKRFox1200) || defined(ARDUINO_SAMD_MKRWAN1300) || defined(ARDUINO_SAMD_MKRWAN1310) \
-      || defined(ARDUINO_SAMD_MKRGSM1400) || defined(ARDUINO_SAMD_MKRNB1500) || defined(ARDUINO_SAMD_MKRVIDOR4000) \
-      || defined(ARDUINO_SAMD_CIRCUITPLAYGROUND_EXPRESS) || defined(__SAMD51__) || defined(__SAMD51J20A__) \
-      || defined(__SAMD51J19A__) || defined(__SAMD51G19A__) || defined(__SAMD51P19A__)  \
-      || defined(__SAMD21E15A__) || defined(__SAMD21E16A__) || defined(__SAMD21E17A__) || defined(__SAMD21E18A__) \
-      || defined(__SAMD21G15A__) || defined(__SAMD21G16A__) || defined(__SAMD21G17A__) || defined(__SAMD21G18A__) \
-      || defined(__SAMD21J15A__) || defined(__SAMD21J16A__) || defined(__SAMD21J17A__) || defined(__SAMD21J18A__) )
-  #error This code is designed to run on SAMD21/SAMD51 platform! Please check your Tools->Board setting.
-#endif
-
-// These define's must be placed at the beginning before #include "SAMDTimerInterrupt.h"
-// _TIMERINTERRUPT_LOGLEVEL_ from 0 to 4
-// Don't define _TIMERINTERRUPT_LOGLEVEL_ > 0. Only for special ISR debugging only. Can hang the system.
-// Don't define TIMER_INTERRUPT_DEBUG > 2. Only for special ISR debugging only. Can hang the system.
-#define TIMER_INTERRUPT_DEBUG         0
-#define _TIMERINTERRUPT_LOGLEVEL_     0
-
-#include "SAMDTimerInterrupt.h"
-#include "SAMD_ISR_Timer.h"
-
-#define HW_TIMER_INTERVAL_MS      10
-
-// Depending on the board, you can select SAMD21 Hardware Timer from TC3, TC4, TC5, TCC, TCC1 or TCC2
-// SAMD51 Hardware Timer only TC3
-
-// Init SAMD timer TIMER_TC3
-SAMDTimer ITimer(TIMER_TC3);
-
-// Init SAMD_ISR_Timer
-// Each SAMD_ISR_Timer can service 16 different ISR-based timers
-SAMD_ISR_Timer ISR_Timer;
 
 #define TIMER_INTERVAL_2MS               2L
 #define TIMER_INTERVAL_1S             1000L
 
-void TimerHandler(void)
-{
-  ISR_Timer.run();
-}
+
 
 int current_row = 0;
 
